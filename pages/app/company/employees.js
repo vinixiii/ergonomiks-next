af70ed9a07f58021ebe2409592c1e211a0fc6ebf
Vinícius Soares da Silva Figueira <47920739870@info.local>
2021-11-11 10:55:44 -0300

    feat: create employees page and fetch data

--- pages/app/company/employees.js
+++ pages/app/company/employees.js
@@ -1,5 +1,9 @@
 import { websitePageHOC } from '../../../src/components/wrappers/WebsitePage/hoc';
 
+import { authService } from '../../../src/services/auth/authService';
+import { api } from '../../../src/components/infra/api';
+import { EmployeesScreen } from '../../../src/components/screens/app/company/EmployeesScreen';
+
 function CompanyEmployeesScreen() {
   return (
     <div style={{ display: 'flex', flex: 1, padding: 40 }}>
@@ -8,4 +12,34 @@ function CompanyEmployeesScreen() {
   );
 }
 
-export default websitePageHOC(CompanyEmployeesScreen);
+export default websitePageHOC(EmployeesScreen);
+
+export async function getServerSideProps(context) {
+  const auth = authService(context);
+  const session = auth.getSession();
+  const token = auth.getToken();
+
+  const employeesListResponse = await api('employee/id/company', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const managersListResponse = await api('manager/id/company', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (employeesListResponse.status === 200) {
+    return {
+      props: {
+        user: session,
+        employees: employeesListResponse.data.data,
+        managers: managersListResponse.data.data,
+      },
+    };
+  }
+}
