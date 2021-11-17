0122617fd3dd60859a444fac8584f582f92c4bd7
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2021-11-17 10:44:40 -0300

    feat: fetch account data to show in preferences page

--- pages/app/preferences.js
+++ pages/app/preferences.js
@@ -3,10 +3,63 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { websitePageHOC } from '../../src/components/wrappers/WebsitePage/hoc';
 import { PreferencesScreen } from '../../src/components/screens/app/PreferencesScreen';
 
+import { authService } from '../../src/services/auth/authService';
+import { api } from '../../src/components/infra/api';
+
 export default websitePageHOC(PreferencesScreen);
 
-export const getStaticProps = async ({ locale }) => ({
-  props: {
-    ...(await serverSideTranslations(locale, ['preferences', 'header'])),
-  },
-});
+export async function getServerSideProps(context) {
+  const auth = authService(context);
+  const session = auth.getSession();
+  const token = auth.getToken();
+
+  let response = {
+    error: 'Failed to fetch data',
+  };
+
+  if (session.role === 'company') {
+    const { data } = await api('company/id/user', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    }).catch((error) => console.log(error));
+
+    response = data.data;
+  }
+
+  if (session.role === 'manager') {
+    const { data } = await api('manager/id/user', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    }).catch((error) => console.log(error));
+
+    response = data.data;
+  }
+
+  if (session.role === 'employee') {
+    const { data } = await api('employee/id/user', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    }).catch((error) => console.log(error));
+
+    response = data.data;
+  }
+
+  console.log('oloco bicho', response);
+
+  return {
+    props: {
+      account: response,
+      user: session,
+      ...(await serverSideTranslations(context.locale, [
+        'preferences',
+        'header',
+      ])),
+    },
+  };
+}
